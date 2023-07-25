062c2903ae5bcd0cb2c201263d8429cf66c8ed35
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-07-25 15:54:43 +0500

    v.1.2 нормализовал модель Billboard, теперь 6 разных моделей, импорт данных происходит за 20 секунд

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,35 +1,89 @@
 from django.db import models
 
 
-class Billboard(models.Model):
-    city = models.CharField(max_length=100, null=True, blank=True)
-    surface_type = models.CharField(max_length=100, null=True, blank=True)
-    osv = models.CharField(max_length=100, null=True, blank=True)
-    side = models.CharField(max_length=100, null=True, blank=True)
-    address = models.CharField(max_length=200, null=True, blank=True)
-    internal_code = models.CharField(max_length=100, null=True, blank=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
-    photo_scheme = models.CharField(max_length=200, null=True, blank=True)
-    price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    digital_impressions = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    grp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    ots = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    espar_code = models.CharField(max_length=100, null=True, blank=True)
-    july_2023 = models.CharField(max_length=100, null=True, blank=True)
-    august_2023 = models.CharField(max_length=100, null=True, blank=True)
-    september_2023 = models.CharField(max_length=100, null=True, blank=True)
-    october_2023 = models.CharField(max_length=100, null=True, blank=True)
-    november_2023 = models.CharField(max_length=100, null=True, blank=True)
-    december_2023 = models.CharField(max_length=100, null=True, blank=True)
-    media_material = models.CharField(max_length=200, null=True, blank=True)
-    product_restrictions = models.CharField(max_length=200, null=True, blank=True)
-    city_district = models.CharField(max_length=100, null=True, blank=True)
-    tech_requirements = models.CharField(max_length=200, null=True, blank=True)
-    installation_price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    reinstallation_price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    software_resolution = models.CharField(max_length=100, null=True, blank=True)
-    note = models.TextField(null=True, blank=True)
+# class Billboard(models.Model):
+#     city = models.CharField(max_length=100, null=True, blank=True, verbose_name="Город")
+#     surface_type = models.CharField(max_length=100, null=True, blank=True, verbose_name="Тип поверхности")
+#     osv = models.CharField(max_length=100, null=True, blank=True, verbose_name="Осв")
+#     side = models.CharField(max_length=100, null=True, blank=True, verbose_name="Сторона")
+#     address = models.CharField(max_length=200, null=True, blank=True, verbose_name="Адрес")
+#     internal_code = models.CharField(max_length=100, null=True, blank=True, verbose_name="Вн. код")
+#     latitude = models.FloatField(null=True, blank=True, verbose_name="Широта")
+#     longitude = models.FloatField(null=True, blank=True, verbose_name="Долгота")
+#     photo_scheme = models.CharField(max_length=200, null=True, blank=True, verbose_name="Фото/схема")
+#     price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Прайс C НДС")
+#     digital_impressions = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True,verbose_name="Диджтал кол-во показов")
+#     grp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="GRP")
+#     ots = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="OTS")
+#     espar_code = models.CharField(max_length=100, null=True, blank=True, verbose_name="Код Эспар")
+#     july_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Июль 2023")
+#     august_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Август 2023")
+#     september_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Сентябрь 2023")
+#     october_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Октябрь 2023")
+#     november_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Ноябрь 2023")
+#     december_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Декабрь 2023")
+#     media_material = models.CharField(max_length=200, null=True, blank=True, verbose_name="Материал носителя")
+#     product_restrictions = models.CharField(max_length=200, null=True, blank=True, verbose_name="Ограничения по продукту")
+#     city_district = models.CharField(max_length=100, null=True, blank=True, verbose_name="Городской Округ")
+#     tech_requirements = models.CharField(max_length=200, null=True, blank=True, verbose_name="Тех. требования")
+#     installation_price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Монтаж. Прайс с НДС")
+#     reinstallation_price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Переклейка. Прайс с НДС")
+#     software_resolution = models.CharField(max_length=100, null=True, blank=True, verbose_name="Разрешение ПО")
+#     note = models.TextField(null=True, blank=True, verbose_name="Примечание")
+#
 
-    class Meta:
-        db_table = 'billboard'
+class CityInfo(models.Model):
+    """Информация о рекламной конструкции в городе"""
+    city = models.CharField(max_length=100, null=True, blank=True, verbose_name="Город")
+    surface_type = models.CharField(max_length=100, null=True, blank=True, verbose_name="Тип поверхности")
+    osv = models.CharField(max_length=100, null=True, blank=True, verbose_name="Осв")
+    side = models.CharField(max_length=100, null=True, blank=True, verbose_name="Сторона")
+    address = models.CharField(max_length=200, null=True, blank=True, verbose_name="Адрес")
+    internal_code = models.CharField(max_length=100, null=True, blank=True, verbose_name="Вн. код")
+    latitude = models.FloatField(null=True, blank=True, verbose_name="Широта")
+    longitude = models.FloatField(null=True, blank=True, verbose_name="Долгота")
+    photo_scheme = models.CharField(max_length=200, null=True, blank=True, verbose_name="Фото/схема")
+
+
+class Metrics(models.Model):
+    """Метрики рекламной конструкции"""
+    billboard = models.ForeignKey(CityInfo, on_delete=models.CASCADE)
+    digital_impressions = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Диджтал кол-во показов")
+    grp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="GRP")
+    ots = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="OTS")
+    espar_code = models.CharField(max_length=100, null=True, blank=True, verbose_name="Код Эспар")
+    july_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Июль 2023")
+    august_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Август 2023")
+    september_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Сентябрь 2023")
+    october_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Октябрь 2023")
+    november_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Ноябрь 2023")
+    december_2023 = models.CharField(max_length=100, null=True, blank=True, verbose_name="Декабрь 2023")
+
+
+class Details(models.Model):
+    """Детали рекламной конструкции"""
+    billboard = models.ForeignKey(CityInfo, on_delete=models.CASCADE)
+    media_material = models.CharField(max_length=200, null=True, blank=True, verbose_name="Материал носителя")
+    product_restrictions = models.CharField(max_length=200, null=True, blank=True, verbose_name="Ограничения по продукту")
+    city_district = models.CharField(max_length=100, null=True, blank=True, verbose_name="Городской Округ")
+    tech_requirements = models.CharField(max_length=200, null=True, blank=True, verbose_name="Тех. требования")
+
+
+class Pricing(models.Model):
+    """Цены на рекламную конструкцию"""
+    billboard = models.ForeignKey(CityInfo, on_delete=models.CASCADE)
+    price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Прайс C НДС")
+    installation_price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Монтаж. Прайс с НДС")
+    reinstallation_price_with_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Переклейка. Прайс с НДС")
+
+
+class Software(models.Model):
+    """ПО рекламной конструкции"""
+    billboard = models.ForeignKey(CityInfo, on_delete=models.CASCADE)
+    software_resolution = models.CharField(max_length=100, null=True, blank=True, verbose_name="Разрешение ПО")
+
+
+class Note(models.Model):
+    """Примечание к рекламной конструкции"""
+    billboard = models.ForeignKey(CityInfo, on_delete=models.CASCADE)
+    note = models.TextField(null=True, blank=True, verbose_name="Примечание")
--- load_data.py
+++ load_data.py
@@ -1,16 +1,105 @@
 import os
 import sys
-from django.core.wsgi import get_wsgi_application
+import pandas as pd
+import django
 
-# Определение абсолютного пути к текущей директории
+from django.core.wsgi import get_wsgi_application
+from django.db import transaction
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+# Настраиваем среду выполнения Django
+django.setup()
+
+# Здесь импортируем модели Django после настройки
+from advertisement.models import (CityInfo,
+                                  Details,
+                                  Metrics,
+                                  Note,
+                                  Pricing,
+                                  Software)
+
 application = get_wsgi_application()
 
-from advertisement.utils import load_data_to_db
+
+def load_data_to_db(file_path):
+    df = pd.read_excel(file_path)
+
+    # Используем списки для bulk_create
+    city_info_list = []
+    metrics_list = []
+    details_list = []
+    pricing_list = []
+    software_list = []
+    note_list = []
+
+    for _, row in df.iterrows():
+        city_billboard_info = CityInfo(
+            city=row['Город'],
+            surface_type=row['Тип поверхности'],
+            osv=row['Осв'],
+            side=row['Сторона'],
+            address=row['Адрес'],
+            internal_code=row['Вн. код'],
+            latitude=row['Широта'],
+            longitude=row['Долгота'],
+            photo_scheme=row.get('фото/схема', ''),
+        )
+        city_info_list.append(city_billboard_info)
+
+        metrics_list.append(Metrics(
+            billboard=city_billboard_info,
+            digital_impressions=row['Диджтал кол-во показов'] if not pd.isna(row['Диджтал кол-во показов']) else None,
+            grp=row['GRP'] if not pd.isna(row['GRP']) else None,
+            ots=row['OTS'] if not pd.isna(row['OTS']) else None,
+            espar_code=row['Код Эспар'],
+            july_2023=row['Июль 2023'],
+            august_2023=row['Август 2023'],
+            september_2023=row['Сентябрь 2023'],
+            october_2023=row['Октябрь 2023'],
+            november_2023=row['Ноябрь 2023'],
+            december_2023=row['Декабрь 2023'],
+        ))
+
+        details_list.append(Details(
+            billboard=city_billboard_info,
+            media_material=row['Материал носителя'],
+            product_restrictions=row['Ограничения по продукту'],
+            city_district=row['Городской Округ'],
+            tech_requirements=row['Тех. требования'],
+        ))
+
+        pricing_list.append(Pricing(
+            billboard=city_billboard_info,
+            price_with_vat=row['Прайс C НДС'] if not pd.isna(row['Прайс C НДС']) else None,
+            installation_price_with_vat=row['Монтаж. Прайс  с НДС'] if not pd.isna(
+                row['Монтаж. Прайс  с НДС']) else None,
+            reinstallation_price_with_vat=row['Переклейка. Прайс с НДС'] if not pd.isna(
+                row['Переклейка. Прайс с НДС']) else None,
+        ))
+
+        software_list.append(Software(
+            billboard=city_billboard_info,
+            software_resolution=row['Разрешение ПО'],
+        ))
+
+        note_list.append(Note(
+            billboard=city_billboard_info,
+            note=row['Примечание'],
+        ))
+
+    # Используем транзакцию для ускорения
+    with transaction.atomic():
+        CityInfo.objects.bulk_create(city_info_list, batch_size=500)
+        Metrics.objects.bulk_create(metrics_list, batch_size=500)
+        Details.objects.bulk_create(details_list, batch_size=500)
+        Pricing.objects.bulk_create(pricing_list, batch_size=500)
+        Software.objects.bulk_create(software_list, batch_size=500)
+        Note.objects.bulk_create(note_list, batch_size=500)
+
 
 if __name__ == "__main__":
-    file_name = "Занятость города 2023.xlsx"
-    file_path = os.path.join(BASE_DIR, file_name)
+    file_path = os.path.join(BASE_DIR, "Занятость города 2023.xlsx")
     load_data_to_db(file_path)
